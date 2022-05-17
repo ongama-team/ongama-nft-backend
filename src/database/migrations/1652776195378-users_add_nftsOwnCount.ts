1a70451df786397d810e6311af25b2b90b823ed2
Christian Balola <christianmugisho17@gmail.com>
2022-05-17 10:57:16 +0200

    Added 1652776195378-users_add_nftsOwnCount migration

---
+++ src/database/migrations/1652776195378-users_add_nftsOwnCount.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class usersAddNftsOwnCount1652776195378 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "users" ADD "nftsOwnCount" int4 DEFAULT 0 AFTER "nftsCount";', undefined);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('ALTER TABLE "users" DROP "nftsOwnCount";', undefined);
+  }
+}
