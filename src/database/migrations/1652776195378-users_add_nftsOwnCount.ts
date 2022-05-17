9c7bf652aa577505da2a9040a8aea5e26a6878c8
Christian Balola <christianmugisho17@gmail.com>
2022-05-17 21:08:27 +0200

    Bug-fix nftsOwnCount migration

--- src/database/migrations/1652776195378-users_add_nftsOwnCount.ts
+++ src/database/migrations/1652776195378-users_add_nftsOwnCount.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class usersAddNftsOwnCount1652776195378 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('ALTER TABLE "users" ADD "nftsOwnCount" int4 DEFAULT 0 AFTER "nftsCount";', undefined);
+    await queryRunner.query('ALTER TABLE "users" ADD "nftsOwnCount" int4 DEFAULT 0;', undefined);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
